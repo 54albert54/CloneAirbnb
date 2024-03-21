c62b7b10b114633ed718950cb687bca6719ea16b
s <as>
2024-03-20 20:32:46 -0400

    Initial Commit

---
+++ cloneAirbnb/cloneAirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloneAirbnbApp.swift
+//  cloneAirbnb
+//
+//  Created by Angel alberto Bernechea on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct cloneAirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
